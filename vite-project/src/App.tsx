f79f8566479e715c600dce5809e903a1c1d8dace
Krati Patel <patelkrati00@gmail.com>
2026-03-28 15:59:04 +0530

    key pair generation

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,9 +1,15 @@
+import { Keypair } from "@solana/web3.js"
 function App() {
+  const createWallet = (): void => {
+    const kp = Keypair.generate();
+    console.log(`keypair is ${kp.publicKey.toString()}`);
+
+  }
 
   return (
     <>
       <h1>Solana Wallet Dashboard</h1>
-      <button>hey</button>
+      <button onClick={createWallet}>create wallet</button>
     </>
   )
 }
