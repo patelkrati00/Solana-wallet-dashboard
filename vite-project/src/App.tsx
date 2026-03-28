f0bcc960a1d0e357f885a948c87e3d1dc51f16a6
Krati Patel <patelkrati00@gmail.com>
2026-03-28 18:41:38 +0530

    fetching balance from a wallet

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,15 +1,31 @@
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui"
-import { useWallet } from "@solana/wallet-adapter-react"
+import { useConnection, useWallet } from "@solana/wallet-adapter-react"
+import { useEffect, useState } from "react";
 
 function App() {
-  const {publicKey} = useWallet();
+  const { publicKey } = useWallet();
+  const { connection } = useConnection();
+  const [balance, setBalance] = useState<number | null>(null);
+
+  useEffect(() => {
+    const getBalance = async () => {
+      if (!publicKey) return;
+      const bal = await connection.getBalance(publicKey);
+      setBalance(bal / 1e9);
+    }
+    getBalance();
+  }, [publicKey])
 
   return (
     <>
       <h1>Solana Wallet Dashboard</h1>
       <WalletMultiButton></WalletMultiButton>
-      {publicKey && 
-      <p>Connected: {publicKey.toString()}</p>}
+      {publicKey &&
+        <>
+          <p>Connected: {publicKey.toString()}</p>
+          <p>Balance: {balance}</p>
+        </>
+      }
     </>
   )
 }
